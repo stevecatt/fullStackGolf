e700b7318141d5129dc40e5d70efbfbc3677dddc
Steve Cattanach <stevecatt@att.net>
2019-04-08 15:04:52 -0500

    dont commit yet

--- app.js
+++ app.js
@@ -75,24 +75,24 @@ app.get('/input-scores', (req, res) => {
 let player='player1'
 let score= 55
 //pushes work the FIFO paft of Quotas, adding latest score to Q1 removing oldest score fromm Q11 
-db.one('SELECT q1,q2,q3,q4,q5,q6,q7,q8,q9,q10,q11 FROM "Quotas" WHERE golfer = $1', [player])
+/*db.one('SELECT q1,q2,q3,q4,q5,q6,q7,q8,q9,q10,q11 FROM "Quotas" WHERE golfer = $1', [player])
 .then ((quota)=>{
   let updatedQuotaList = Object.values(quota)
   
-  //console.log(updatedQuotaList)
+  
   updatedQuotaList.unshift(score)
-  //console.log(updatedQuotaList)
+  
   updatedQuotaList.pop()
   updatedQuotaList.push(player)
   db.none('UPDATE "Quotas" SET q1=$1,q2=$2,q3=$3,q4=$4,q5=$5,q6=$6,q7=$7,q8=$8,q9=$9,q10=$10,q11=$11 WHERE golfer=$12',updatedQuotaList)
 
   
   
-  //console.log(updatedQuotaList[11])
-
-  //console.log(updatedQuotaList)
+  
 
 })
+*/
+
 
 app.listen(3000,function(){
   console.log("node server has started")
--- routes/input-scores.js
+++ routes/input-scores.js
@@ -14,6 +14,32 @@ router.post('/add-player', (req, res) => {
   let score = parseInt(req.body.score)
   console.log(playerName)
 
+  db.any('SELECT golfer FROM "Quotas" WHERE golfer=$1',[playerName])
+  .then((player)=>{
+   if(player){
+     console.log(player)
+     db.one('SELECT q1,q2,q3,q4,q5,q6,q7,q8,q9,q10,q11 FROM "Quotas" WHERE golfer = $1', [player])
+.then ((quota)=>{
+  let updatedQuotaList = Object.values(quota)
+  
+  
+  updatedQuotaList.unshift(score)
+  
+  updatedQuotaList.pop()
+  updatedQuotaList.push(player)
+  db.none('UPDATE "Quotas" SET q1=$1,q2=$2,q3=$3,q4=$4,q5=$5,q6=$6,q7=$7,q8=$8,q9=$9,q10=$10,q11=$11 WHERE golfer=$12',updatedQuotaList)
+
+  
+  
+  
+
+})
+
+   }
+  })
+})
+/*
+
   db.one('INSERT INTO "Quotas"(golfer, q1) VALUES($1, $2) RETURNING id', [playerName, score])
   .then((data) => {
     console.log(data)
@@ -21,5 +47,5 @@ router.post('/add-player', (req, res) => {
   }).catch(error => console.log(error))
   res.render('input-scores', {message: "The player's score has been added."})
  })
-
+*/
 module.exports = router
