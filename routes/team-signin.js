9eca5e8299cb5161da529282db7124362c9bebcd
Steve Cattanach <stevecatt@att.net>
2019-04-09 17:32:49 -0500

    added login and scoring options

--- routes/team-signin.js
+++ routes/team-signin.js
@@ -13,15 +13,27 @@ router.get('/team-sign-in',(req,res)=>{
 router.post('/team-sign-in',(req,res)=>{
     let teamNumber=parseInt(req.body.teamNumber)
     let password = req.body.password
+    let week = parseInt(req.body.week)
     console.log(teamNumber)
     console.log(password)
-    db.one('SELECT hash FROM teams WHERE team = $1',[teamNumber])
+    db.one('SELECT team,hash, player1,player2 FROM teams WHERE team = $1',[teamNumber])
     .then((hash)=>{
-        console.log(hash.hash)
+        console.log(hash)
         bcrypt.compare(password,hash.hash,function(err,result){
             if (result==true){
                 console.log("success")
-                res.redirect('/input-scores')
+                //going to add a query to schedule by week to find players
+                let team1Player1=hash.player1
+                let team1Player2=hash.player2
+                let team1number=hash.team
+                //this is hardcode for now
+                let team2Number=30
+                let team2Player1="billy"
+                let team2Player2="bob"
+
+                let teams = {week:week,team1Number:team1number,team1Player1:team1Player1, team1Player2:team1Player2,team2Number:team2Number,team2Player1:team2Player1,team2Player2:team2Player2}
+                
+                res.render('input-scores',teams)
                 
             }else{
                 console.log("wrong Password")
