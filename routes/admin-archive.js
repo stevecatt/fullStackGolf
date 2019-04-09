3f110ba2173c08938d236a84f3f66311a69c826e
Shaun W Colley <swc7838@gmail.com>
2019-04-09 10:39:36 -0500

    can now archive teams to a table

--- routes/admin-archive.js
+++ routes/admin-archive.js
@@ -10,12 +10,26 @@ router.use(bodyParser.urlencoded({ extended: false }))
 router.get('/admin/archive-teams', (req,res) => {
   res.render('admin-archive')
 })
+
+let year = new Date().getFullYear().toString()
+let archivedTeams = year + "archivedteams"
+
 router.post('/admin/archive-teams', (req,res) => {
-  let year = new Date().getFullYear().toString()
-  let archivedTeams = year + "-archived-teams"
-  db.none('SELECT * INTO $1 FROM teams;', [archivedTeams])
+  db.none('SELECT * INTO "$1" FROM teams;', [archivedTeams])
   .then(() => {
-    res.render('admin-archive', {message: "Successfully archived Current Season's teams"})
+    db.none('DELETE FROM teams;').then(() => {
+          res.render('admin-archive', {message1: "Successfully archived Current Season's teams."})
+    })
+  })
+})
+
+router.post('/admin/undo-archive', (req,res) => {
+  db.none('INSERT INTO teams SELECT * FROM "$1";', [archivedTeams])
+  .then(() => {
+    db.none('DROP TABLE "$1";', [archivedTeams])
+    .then(() => {
+          res.render('admin-archive', {message2: "Successfully undid archive."})
+    })
   })
 })
 
