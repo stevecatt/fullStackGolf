d389409b676a729be396186b4ec94759a3b886f9
Shaun W Colley <swc7838@gmail.com>
2019-04-11 11:51:14 -0500

    can now calculate a no show score

--- routes/scoring.js
+++ routes/scoring.js
@@ -26,12 +26,12 @@ function getTeams(){
             ABPlayer(team,teamPlayer1,teamPlayer2)
 
         }
-        
-           
+
+
 
         })
-        
-    
+
+
 }
 
 
@@ -42,33 +42,33 @@ function ABPlayer(team,teamPlayer1,teamPlayer2){
     let teamNumber=team
     let thisweekplayer1 = thisWeeksQuotas.filter(quota=>quota.name==teamPlayer1)
     let thisweekplayer2 = thisWeeksQuotas.filter(quota=>quota.name==teamPlayer2)
-   
+
     if(thisweekplayer1[0].quota >= thisweekplayer2[0].quota){
          APlayer= teamPlayer1
          BPlayer= teamPlayer2
          APlayerQuota=thisweekplayer1[0].quota
          BPlayerQuota=thisweekplayer2[0].quota
-        
+
 
     }else{
          APlayer=teamPlayer2
          BPlayer=teamPlayer1
          APlayerQuota=thisweekplayer2[0].quota
          BPlayerQuota=thisweekplayer1[0].quota
-       
+
     }
 
    let ABPlayersPush = {teamNumber:teamNumber,APlayer:APlayer, APlayerQuota:APlayerQuota,BPlayer:BPlayer,BPlayerQuota:BPlayerQuota}
    ABPlayers.push(ABPlayersPush)
-   
-    
+
+
     }
 
 router.get('/next-weeks-matches',(req,res)=>{
-    
+
     getTeams()
 
-    
+
 
     res.render('next-weeks-matches',{ABPlayers:ABPlayers})
 
